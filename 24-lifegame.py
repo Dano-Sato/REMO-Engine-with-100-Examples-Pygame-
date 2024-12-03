30d47707d15871ccb302ddda3d40b6cc64379282
Dano Sato <ten0102@naver.com>
2024-12-03 13:44:24 +0900

    life Game

---
+++ 24-lifegame.py
@@ -0,0 +1,105 @@
+from REMOLib import *
+
+
+
+
+
+
+#게임 오브젝트들을 선언하는 곳입니다.
+class Obj:
+    None
+
+class cellState(Enum):
+    DEAD = 0
+    ALIVE = 1
+
+
+class mainScene(Scene):
+    grid_size = 50
+    
+    def initOnce(self):
+        # 그리드 초기화
+        self.grid = gridObj(RPoint(100,100), tileSize=(20,20), grid=(self.grid_size,self.grid_size))
+
+        
+        # 초기 패턴 설정 (글라이더)
+        self.grid[2][1].state = cellState.ALIVE  # (1,2)
+        self.grid[3][2].state = cellState.ALIVE  # (2,3)
+        self.grid[1][3].state = cellState.ALIVE  # (3,1)
+        self.grid[2][3].state = cellState.ALIVE  # (3,2)
+        self.grid[3][3].state = cellState.ALIVE  # (3,3)
+        return
+    
+    def getState(self,x,y):
+        if not hasattr(self.grid[y][x],'state'):
+            self.grid[y][x].state = cellState.DEAD
+        return self.grid[y][x].state
+    
+    def setState(self,x,y,state):
+        self.grid[y][x].state = state
+        if state == cellState.DEAD:
+            self.grid[y][x].color = Cs.grey
+        else:
+            self.grid[y][x].color = Cs.white
+        return
+
+    def count_neighbors(self, x, y):
+        count = 0
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+                nx = (x + dx) % self.grid_size
+                ny = (y + dy) % self.grid_size
+                if self.getState(nx,ny) == cellState.ALIVE:
+                    count += 1
+        return count
+
+    def update(self):
+        next_state = {}
+        
+        # 다음 세대 계산
+        for y in range(self.grid_size):
+            for x in range(self.grid_size):
+                neighbors = self.count_neighbors(x, y)
+                
+                if self.getState(x,y) == cellState.ALIVE:  # 살아있는 셀
+                    if neighbors < 2 or neighbors > 3:
+                        next_state[(x,y)] = cellState.DEAD
+                else:  # 죽은 셀
+                    if neighbors == 3:
+                        next_state[(x,y)] = cellState.ALIVE
+        
+        # 그리드 업데이트
+        for y in range(self.grid_size):
+            for x in range(self.grid_size):
+                if (x,y) in next_state:
+                    self.setState(x,y,next_state[(x,y)])
+        return
+
+    def draw(self):
+        self.grid.draw()
+        return
+
+
+class defaultScene(Scene):
+    def initOnce(self):
+        return
+    def init(self):
+        return
+    def update(self):
+        return
+    def draw(self):
+        return
+
+class Scenes:
+    mainScene = mainScene()
+
+
+if __name__=="__main__":
+    #Screen Setting
+    window = REMOGame(window_resolution=(1920,1080),screen_size=(2560,1440),fullscreen=False,caption="LIFE GAME")
+    window.setCurrentScene(Scenes.mainScene)
+    window.run()
+
+    # Done! Time to quit.
