1b1935b4095bcdb1a22ae70d9335f882e3986885
Dano Sato <uchang01@gmail.com>
2025-09-21 22:00:21 +0900

    particlePreset 적용

--- 22-particleShowcase.py
+++ 22-particleShowcase.py
@@ -1,4 +1,5 @@
 from REMOLib import *
+from REMOLib.particle import particledefaultPreset
 from REMOLib.particles import ParticleEmitter
 
 import time
@@ -24,20 +25,22 @@ class mainScene(Scene):
             textObj("- 우클릭: 연기 파티클", pos=(60, 200), size=20, color=Cs.white),
         ]
 
-        self.trail_emitter = ParticleEmitter(RPoint(600, 400), max_particles=240)
-        self.trail_emitter.set_defaults(
-            speed_range=(120.0, 220.0),
-            direction_range=(200.0, 340.0),
-            lifetime_range=(0.25, 0.45),
-            size_range=(4.0, 8.0),
-            color_range=((220, 255), (180, 240), (120, 200)),
-            alpha_range=(140, 255),
-            gravity=420.0,
-            position_jitter=((-12.0, 12.0), (-12.0, 12.0)),
+        self.trail_emitter = ParticleEmitter(
+            RPoint(600, 400),
+            max_particles=240,
+            defaults=particleDefaultPreset.magic_glitter(),
         )
 
-        self.explosion_emitter = ParticleEmitter(RPoint(0, 0), max_particles=320)
-        self.smoke_emitter = ParticleEmitter(RPoint(0, 0), max_particles=260)
+        self.explosion_emitter = ParticleEmitter(
+            RPoint(0, 0),
+            max_particles=320,
+            defaults=particleDefaultPreset.explosion_fireball(),
+        )
+        self.smoke_emitter = ParticleEmitter(
+            RPoint(0, 0),
+            max_particles=260,
+            defaults=particleDefaultPreset.heavy_smoke(),
+        )
 
     def update(self):
 
@@ -47,33 +50,10 @@ class mainScene(Scene):
         self.trail_emitter.emit(5, position=mouse_pos)
 
         if Rs.userJustLeftClicked():
-            self.explosion_emitter.emit(
-                90,
-                position=mouse_pos,
-                speed_range=(180.0, 360.0),
-                direction_range=(0.0, 360.0),
-                lifetime_range=(0.6, 1.0),
-                size_range=(10.0, 22.0),
-                color_range=((220, 255), (120, 200), (40, 120)),
-                alpha_range=(200, 255),
-                gravity=420.0,
-                position_jitter=((-12.0, 12.0), (-12.0, 12.0)),
-            )
+            self.explosion_emitter.emit(90, position=mouse_pos)
 
         if Rs.userJustRightClicked():
-            self.smoke_emitter.emit(
-                55,
-                position=mouse_pos,
-                velocity_range=((-40.0, 40.0), (-120.0, -40.0)),
-                lifetime_range=(1.1, 1.6),
-                size_range=(18.0, 30.0),
-                color_range=((110, 160), (110, 160), (110, 160)),
-                alpha_range=(70, 140),
-                gravity=-60.0,
-                fade=True,
-                shrink=False,
-                position_jitter=((-20.0, 20.0), (-10.0, 10.0)),
-            )
+            self.smoke_emitter.emit(55, position=mouse_pos)
 
         self.trail_emitter.update()
         self.explosion_emitter.update()
